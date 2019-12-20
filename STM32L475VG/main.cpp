e1064fce10bde24138bf743d5b46f4bbb6b42865
perry0513 <perrypeiweichen@gmail.com>
2019-12-20 20:30:59 +0800

    able to sample data, but error occurs when sending

--- STM32L475VG/main.cpp
+++ STM32L475VG/main.cpp
@@ -1,5 +1,5 @@
-/* WiFi Example
- * Copyright (c) 2016 ARM Limited
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2015 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,105 +14,420 @@
  * limitations under the License.
  */
 
-#include "mbed.h"
-#include "wifi-ism43362/ISM43362Interface.h"
+#include <events/mbed_events.h>
+#include <mbed.h>
+#include "ble/BLE.h"
+#include "ble/gap/Gap.h"
+#include "ble/services/HeartRateService.h"
+#include "MyService.h"
+#include "pretty_printer.h"
+#include "stm32l475e_iot01_accelero.h"
+#include "stm32l475e_iot01_gyro.h"
+
+const static char DEVICE_NAME[] = "Hockey";
 
 Serial pc(USBTX, USBRX);
-// WiFiInterface *wifi;
-WiFiInterface *wifi = new ISM43362Interface;
+Semaphore sem(0);
 
-const char *sec2str(nsapi_security_t sec)
-{
-    switch (sec) {
-        case NSAPI_SECURITY_NONE:
-            return "None";
-        case NSAPI_SECURITY_WEP:
-            return "WEP";
-        case NSAPI_SECURITY_WPA:
-            return "WPA";
-        case NSAPI_SECURITY_WPA2:
-            return "WPA2";
-        case NSAPI_SECURITY_WPA_WPA2:
-            return "WPA/WPA2";
-        case NSAPI_SECURITY_UNKNOWN:
-        default:
-            return "Unknown";
-    }
-}
+Thread t_sensor;
+Thread t_ble;
 
-int scan_demo(ISM43362Interface *wifi)
-{
-    WiFiAccessPoint *ap;
+static events::EventQueue event_queue(/* event count */ 16 * EVENTS_EVENT_SIZE);
 
-    printf("Scan:\n");
-
-    int count = wifi->scan(NULL,0);
-    printf("%d\n", count);
-
-    if (count <= 0) {
-        printf("scan() failed with return value: %d\n", count);
-        return 0;
+class Sensors {
+#define SCALE_MULTIPLIER    0.045
+#define TIMESTEP            0.0005
+public:
+    Sensors(events::EventQueue &event_queue) : _event_queue(event_queue) {
+        BSP_ACCELERO_Init();    
+        BSP_GYRO_Init();
+        align();
+        _event_queue.call_every(1, this, &Sensors::update);
     }
 
-    /* Limit number of network arbitrary to 15 */
-    count = count < 15 ? count : 15;
+    void calculate(float * pGyroDataXYZ, int16_t * pAccDataXYZ) {
+        for (int i = 0; i < 3; ++i) {
+            if (abs(pGyroDataXYZ[i]) > 15)
+                _angle[i] += (pGyroDataXYZ[i] + _GyroAccumulate[i]) / 2 * TIMESTEP * SCALE_MULTIPLIER;
+            if (abs(pAccDataXYZ[i]) > 10) {
+                _velocity[i] += (pAccDataXYZ[i] + _AccAccumulate[i]) / 2 * TIMESTEP;
+                _position[i] += (pAccDataXYZ[i] + _AccAccumulate[i]) / 2 * TIMESTEP * TIMESTEP / 2;
+            } else _velocity[i] = 0;
 
-    ap = new WiFiAccessPoint[count];
-    count = wifi->scan(ap, count);
+            // if (abs(pAccDataXYZ[i] - AccAccumulate[i]) < 2)
+            //     velocity[i] = 0;
+            
+            _position[i] += _velocity[i] * TIMESTEP;
+        }
 
-    if (count <= 0) {
-        printf("scan() failed with return value: %d\n", count);
-        return 0;
+        for (int i = 0; i < 3; ++i) {
+            _GyroAccumulate[i] = pGyroDataXYZ[i];
+            _AccAccumulate[i] = pAccDataXYZ[i];
+        }
+
     }
 
-    for (int i = 0; i < count; i++) {
-        printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\n", ap[i].get_ssid(),
-               sec2str(ap[i].get_security()), ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
-               ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5], ap[i].get_rssi(), ap[i].get_channel());
-    }
-    printf("%d networks available.\n", count);
+    void align() {
+        pc.printf("Aligning...\n");
 
-    delete[] ap;
-    return count;
+        while (_sample_num < 2000) {
+            _sample_num++;
+            BSP_GYRO_GetXYZ(_pGyroDataXYZ);
+            BSP_ACCELERO_AccGetXYZ(_pAccDataXYZ);
+            for (int i = 0; i < 3; ++i) {
+                _GyroOffset[i] += _pGyroDataXYZ[i];
+                _AccOffset[i] += _pAccDataXYZ[i];
+            }
+
+            wait(TIMESTEP);
+        }
+
+        for (int i = 0; i < 3; ++i)
+            pc.printf("%d ", _AccOffset[i]);
+        pc.printf("\n");
+
+        for (int i = 0; i < 3; ++i) {
+            _GyroOffset[i] /= _sample_num;
+            _AccOffset[i] /= _sample_num;
+        }
+
+        for (int i = 0; i < 3; ++i)
+            pc.printf("%d ", _AccOffset[i]);
+        pc.printf("\n");
+
+        pc.printf("Done alignment\n");
+        _sample_num = 0;
+    }
+
+    void update() {
+        pc.printf("Waiting at Sensors::update...\n");
+        sem.wait();
+        pc.printf("Enter Sensors::update\n");
+        // pc.printf("Start updating...\n");
+        // socket.set_blocking(1);
+
+
+        // while (1){
+        ++_sample_num;
+
+        BSP_GYRO_GetXYZ(_pGyroDataXYZ);
+        BSP_ACCELERO_AccGetXYZ(_pAccDataXYZ);
+        for (int i = 0; i < 3; ++i) {
+            _pGyroDataXYZ[i] = (_pGyroDataXYZ[i] - _GyroOffset[i]) * SCALE_MULTIPLIER;
+            _pAccDataXYZ[i] = _pAccDataXYZ[i] - _AccOffset[i];
+        }
+
+        // if (0) {
+        if (_sample_num % 100 == 0) {
+            // pc.printf("\nGYRO_X = %.2f\n", pGyroDataXYZ[0]);
+            // pc.printf("GYRO_Y = %.2f\n", pGyroDataXYZ[1]);
+            // pc.printf("GYRO_Z = %.2f\n", pGyroDataXYZ[2]);
+
+            // pc.printf("\nANGLE_X = %f\n", angle[0]);
+            // pc.printf("ANGLE_Y = %f\n", angle[1]);
+            pc.printf("ANGLE_Z = %f\n", _angle[2]);
+
+            // pc.printf("\nACCELERO_X = %d\n", pAccDataXYZ[0]);
+            // pc.printf("ACCELERO_Y = %d\n", pAccDataXYZ[1]);
+            // pc.printf("ACCELERO_Z = %d\n", pAccDataXYZ[2]);
+
+            // pc.printf("\nVELOCITY_X = %f\n", velocity[0]);
+            // pc.printf("VELOCITY_Y = %f\n", velocity[1]);
+            // pc.printf("VELOCITY_Z = %f\n", velocity[2]);
+
+            // pc.printf("\nPOSITION_X = %f\n", position[0]);
+            // pc.printf("POSITION_Y = %f\n", position[1]);
+            // pc.printf("POSITION_Z = %f\n", position[2]);
+        }
+
+        if (_sample_num % 100 == 0) {
+            if (abs(_velocity[0]) > 1) {
+                if (_velocity[0] > 0)
+                    pc.printf("%5s ", "left");
+                else pc.printf("%5s ", "right");
+            } else pc.printf("still ");
+
+            if (abs(_velocity[1]) > 1) {
+                if (_velocity[1] > 0)
+                    pc.printf("%5s\n", "back");
+                else pc.printf("%5s\n", "front");
+            } else pc.printf("still\n");
+
+            pc.printf("\n");
+        }
+        
+        wait(TIMESTEP);
+    
+        calculate(_pGyroDataXYZ, _pAccDataXYZ);
+
+        // }
+        sem.release();
+        pc.printf("Release sem at Sensors::udpate\n");
+    }
+
+    void getDirection(uint8_t& right, uint8_t& up) {
+        // if (sample_num % 100 == 0) {
+        pc.printf("In Sensors::getDirection...\n");
+
+        pc.printf("%f\n", abs(_velocity[0]));
+        if  (abs(_velocity[0]) > 1.0)
+            pc.printf("%f\n", _velocity[0]);
+        else pc.printf("ha1\n");
+
+        if  (abs(_velocity[0]) > 1)
+            pc.printf("%f\n", _velocity[0]);
+        else pc.printf("ha2\n");
+/*        
+        if (abs(_velocity[0]) > 1) {
+            if (_velocity[0] > 0) {
+                pc.printf("%5s ", "left");
+                right = 2;
+            }
+            else {
+                pc.printf("%5s ", "right");
+                right = 1;
+            }
+        } else {
+            pc.printf("still ");
+            right = 0;
+        }
+
+        if (abs(_velocity[1]) > 1) {
+            if (_velocity[1] > 0) {
+                pc.printf("%5s\n", "back");
+                up = 2;
+            }
+            else {
+                pc.printf("%5s\n", "front");
+                up = 1;
+            }
+        } else {
+            pc.printf("still\n");
+            up = 0;
+        }
+
+        pc.printf("\n");
+        
+        // }
+*/
+    }
+
+    // returns angle / 2 due to 8 bit
+    void getAngle(uint8_t& angle) {
+        angle = int(_angle[2]) / 2;
+    }
+
+private:
+    float _angle[3] = {};
+    float _velocity[3] = {};
+    float _position[3] = {};
+
+    float _GyroAccumulate[3] = {};
+    float _AccAccumulate[3] = {};
+
+    int   _sample_num = 0;
+    int16_t _pAccDataXYZ[3] = {0};
+    float _pGyroDataXYZ[3] = {0};
+
+    int   _AccOffset[3] = {};
+    float _GyroOffset[3] = {};
+
+    events::EventQueue &_event_queue;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class MyDemo : ble::Gap::EventHandler {
+public:
+    MyDemo(BLE &ble, events::EventQueue &event_queue, uint8_t right, uint8_t up, uint8_t angle, Sensors * sensor) :
+        _ble(ble),
+        _event_queue(event_queue),
+        _led1(LED1, 1),
+        _connected(false),
+        _uuid(GattService::UUID_MY_SERVICE),
+        _service(ble, right, up, angle),
+        _adv_data_builder(_adv_buffer) { }
+
+    void start() {
+        _ble.gap().setEventHandler(this);
+
+        _ble.init(this, &MyDemo::on_init_complete);
+
+        _event_queue.call_every(500, this, &MyDemo::blink);
+        pc.printf("_event_queue setup blink\n");
+        _event_queue.call_every(1, this, &MyDemo::send_sensor_value);
+        pc.printf("_event_queue setup update_sensor_value\n");
+
+        // _event_queue.dispatch_forever();
+        // pc.printf("_event_queue dispatch forever\n");
+    }
+
+private:
+    /** Callback triggered when the ble initialization process has finished */
+    void on_init_complete(BLE::InitializationCompleteCallbackContext *params) {
+        if (params->error != BLE_ERROR_NONE) {
+            pc.printf("Ble initialization failed.");
+            return;
+        }
+
+        print_mac_address();
+
+        start_advertising();
+        pc.printf("Advertising...\n");
+    }
+
+    void start_advertising() {
+        /* Create advertising parameters and payload */
+
+        ble::AdvertisingParameters adv_parameters(
+            ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
+            ble::adv_interval_t(ble::millisecond_t(1000))
+        );
+
+        _adv_data_builder.setFlags();
+        _adv_data_builder.setAppearance(ble::adv_data_appearance_t::GENERIC_HEART_RATE_SENSOR);
+        _adv_data_builder.setLocalServiceList(mbed::make_Span(&_uuid, 1));
+        _adv_data_builder.setName(DEVICE_NAME);
+
+        /* Setup advertising */
+
+        ble_error_t error = _ble.gap().setAdvertisingParameters(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            adv_parameters
+        );
+
+        if (error) {
+            pc.printf("_ble.gap().setAdvertisingParameters() failed\r\n");
+            return;
+        }
+
+        error = _ble.gap().setAdvertisingPayload(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            _adv_data_builder.getAdvertisingData()
+        );
+
+        if (error) {
+            pc.printf("_ble.gap().setAdvertisingPayload() failed\r\n");
+            return;
+        }
+
+        /* Start advertising */
+
+        error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+        if (error) {
+            pc.printf("_ble.gap().startAdvertising() failed\r\n");
+            return;
+        }
+    }
+
+    void send_sensor_value() {
+        pc.printf("%d\n", _connected);
+        if (_connected) {
+            pc.printf("haha\n");
+            // Do blocking calls or whatever is necessary for sensor polling.
+            // In our case, we simply update the HRM measurement.
+            // _hr_counter++;
+
+            // //  100 <= HRM bps <=175
+            // if (_hr_counter == 175) {
+            //     _hr_counter = 100;
+            // }
+
+            uint8_t right, up, angle;
+
+            pc.printf("Waiting at MyDemo::send_sensor_value...\n");
+            sem.wait();
+            pc.printf("Enter MyDemo::send_sensor_value\n");
+            
+            pc.printf("Getting dierction info...\n");
+            _sensor->getDirection(right, up);
+            pc.printf("Getting angle info...\n");
+            _sensor->getAngle(angle);
+            
+            sem.release();
+            pc.printf("Release sem at MyDemo::send_sensor_value\n");
+
+            pc.printf("Updating info...\n");
+            _service.updateInfo(right, up, angle);
+            pc.printf("%d, %d, %d\n", right, up, angle);
+        }
+    }
+
+    void blink(void) {
+        _led1 = !_led1;
+    }
+
+private:
+    /* Event handler */
+
+    void onDisconnectionComplete(const ble::DisconnectionCompleteEvent&) {
+        _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+        _connected = false;
+    }
+
+    virtual void onConnectionComplete(const ble::ConnectionCompleteEvent &event) {
+        if (event.getStatus() == BLE_ERROR_NONE) {
+            pc.printf("Successfully connected\n");
+            _connected = true;
+        } else pc.printf("Failed connecting\n");
+    }
+
+private:
+    BLE &_ble;
+    events::EventQueue &_event_queue;
+    DigitalOut _led1;
+
+    Sensors * _sensor;
+    bool _connected;
+
+    UUID _uuid;
+
+    MyService _service;
+
+    uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
+    ble::AdvertisingDataBuilder _adv_data_builder;
+};
+
+/** Schedule processing of events from the BLE middleware in the event queue. */
+void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context) {
+    event_queue.call(Callback<void()>(&context->ble, &BLE::processEvents));
 }
 
 int main()
-{    
+{
     pc.baud(115200);
-    printf("WiFi example\n");
+    pc.printf("Program start\n");
 
-#ifdef MBED_MAJOR_VERSION
-    printf("Mbed OS version %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
-#endif
+    Sensors sensor(event_queue);
 
+    BLE &ble = BLE::Instance();
+    ble.onEventsToProcess(schedule_ble_events);
 
-    wifi = WiFiInterface::get_default_instance();
-    if (!wifi) {
-        printf("ERROR: No WiFiInterface found.\n");
-        return -1;
-    }
+    MyDemo demo(ble, event_queue, 0, 0, 0, &sensor);
+    demo.start();
+    pc.printf("Demo started!\n");
 
-    // int count = scan_demo(wifi);
-    // if (count == 0) {
-    //     printf("No WIFI APs found - can't continue further.\n");
-    //     return -1;
-    // }
+    pc.printf("Start dispatching...\n");
 
-    printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
-    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-    if (ret != 0) {
-        printf("\nConnection error: %d\n", ret);
-        return -1;
-    }
+    // t_sensor.start(callback(&Sensors::update));
+    event_queue.dispatch_forever();
 
-    printf("Success\n\n");
-    printf("MAC: %s\n", wifi->get_mac_address());
-    printf("IP: %s\n", wifi->get_ip_address());
-    printf("Netmask: %s\n", wifi->get_netmask());
-    printf("Gateway: %s\n", wifi->get_gateway());
-    printf("RSSI: %d\n\n", wifi->get_rssi());
-
-    wifi->disconnect();
-
-    printf("\nDone\n");
+    return 0;
 }
+
